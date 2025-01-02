c8f0255d28538c75ad00dc6b72efee1eea34296d
DoubledConG <992196871@qq.com>
2025-01-02 21:30:01 +0800

    feat: 调整响应式布局

--- app/page.js
+++ app/page.js
@@ -6,20 +6,48 @@ const ResponsiveGridLayout = WidthProvider(Responsive);
 
 function FeatureGrid() {
   const layouts = {
-    lg:[
+    xxs:[
       { i: "1", x: 0, y: 0, w: 2, h: 2 },
-      { i: "2", x: 2, y: 0, w: 2, h: 2 },
-      { i: "3", x: 4, y: 0, w: 2, h: 2 },
-      { i: "4", x: 0, y: 2, w: 3, h: 2 },
-      { i: "5", x: 3, y: 2, w: 3, h: 2 }
+      { i: "2", x: 0, y: 2, w: 1, h: 1 },
+      { i: "3", x: 2, y: 2, w: 1, h: 1 },
+      { i: "4", x: 0, y: 4, w: 2, h: 1 },
+      { i: "5", x: 0, y: 5, w: 2, h: 1 }
     ],
+    xs:[
+      { i: "1", x: 0, y: 0, w: 4, h: 2 },
+      { i: "2", x: 4, y: 0, w: 4, h: 1 },
+      { i: "3", x: 4, y: 2, w: 4, h: 1 },
+      { i: "4", x: 0, y: 2, w: 4, h: 2 },
+      { i: "5", x: 4, y: 2, w: 4, h: 2 }
+    ],
+    sm:[
+      { i: "1", x: 0, y: 0, w: 4, h: 2 },
+      { i: "2", x: 4, y: 0, w: 4, h: 2 },
+      { i: "3", x: 8, y: 0, w: 4, h: 2 },
+      { i: "4", x: 0, y: 2, w: 6, h: 2 },
+      { i: "5", x: 6, y: 2, w: 6, h: 2 }
+    ],
+    md:[
+      { i: "1", x: 0, y: 0, w: 4, h: 2 },
+      { i: "2", x: 4, y: 0, w: 4, h: 2 },
+      { i: "3", x: 8, y: 0, w: 4, h: 2 },
+      { i: "4", x: 0, y: 2, w: 6, h: 2 },
+      { i: "5", x: 6, y: 2, w: 6, h: 2 }
+    ],
+    lg:[
+      { i: "1", x: 0, y: 0, w: 4, h: 2 },
+      { i: "2", x: 4, y: 0, w: 4, h: 2 },
+      { i: "3", x: 8, y: 0, w: 4, h: 2 },
+      { i: "4", x: 0, y: 2, w: 6, h: 2 },
+      { i: "5", x: 6, y: 2, w: 6, h: 2 }
+    ]
   }
   return (
     <ResponsiveGridLayout
       className="layout"
       layouts={layouts}
-      breakpoints={{ lg: 1536, md: 1280, sm: 1024, xs: 768, xxs: 640 }}
-      cols={{ lg: 8, md: 6, sm: 4, xs: 2, xxs: 1 }}
+      breakpoints={{ lg: 1536, md: 1280, sm: 1024, xs: 768, xxs: 0 }}
+      cols={{ lg: 12, md: 12, sm: 12, xs: 8, xxs: 2 }}
     >
       <div key="1" className=" flex justify-center items-center font-bold text-3xl bg-green-50">1</div>
       <div key="2" className=" flex justify-center items-center font-bold text-3xl bg-blue-50">2</div>
